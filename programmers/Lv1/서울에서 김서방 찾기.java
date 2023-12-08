cc75b15fa549b9df389809ab2c57bdfa9092ad16
son <152856285+je-s0n@users.noreply.github.com>
2023-12-08 13:21:46 +0900

    Create 서울에서 김서방 찾기.java

---
+++ programmers/Lv1/서울에서 김서방 찾기.java
@@ -0,0 +1,12 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] seoul) {
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < seoul.length; i++) {
+            map.put(seoul[i], i);
+        }
+        return "김서방은 " + map.get("Kim") + "에 있다";
+    }
+}
